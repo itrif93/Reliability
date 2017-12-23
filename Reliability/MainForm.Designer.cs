28000c8e3e3516b74d432f743ca2317b167cc9d6
itrif93 <31711385+itrif93@users.noreply.github.com>
2017-12-23 20:34:47 +0300

    initial comment

---
+++ Reliability/MainForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Acer
+ * Date: 30.09.2017
+ * Time: 17:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+namespace LR1
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ComboBox comboBox3;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.ComboBox comboBox4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.ComboBox comboBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label6;
+		private System.Windows.Forms.TextBox textBox1;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.comboBox3 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.comboBox4 = new System.Windows.Forms.ComboBox();
+			this.label4 = new System.Windows.Forms.Label();
+			this.comboBox5 = new System.Windows.Forms.ComboBox();
+			this.label5 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label6 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(12, 13);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(134, 23);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Вид деятельности:";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(253, 10);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(121, 21);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(253, 36);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(121, 21);
+			this.comboBox2.TabIndex = 3;
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(13, 39);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(133, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "Располагаемое время:";
+			// 
+			// comboBox3
+			// 
+			this.comboBox3.FormattingEnabled = true;
+			this.comboBox3.Location = new System.Drawing.Point(253, 63);
+			this.comboBox3.Name = "comboBox3";
+			this.comboBox3.Size = new System.Drawing.Size(121, 21);
+			this.comboBox3.TabIndex = 5;
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(13, 63);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(162, 23);
+			this.label3.TabIndex = 4;
+			this.label3.Text = "Характеристики оператора:";
+			// 
+			// comboBox4
+			// 
+			this.comboBox4.FormattingEnabled = true;
+			this.comboBox4.Location = new System.Drawing.Point(253, 90);
+			this.comboBox4.Name = "comboBox4";
+			this.comboBox4.Size = new System.Drawing.Size(121, 21);
+			this.comboBox4.TabIndex = 7;
+			// 
+			// label4
+			// 
+			this.label4.Location = new System.Drawing.Point(12, 88);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(163, 23);
+			this.label4.TabIndex = 6;
+			this.label4.Text = "Характеристики обстановки:";
+			// 
+			// comboBox5
+			// 
+			this.comboBox5.FormattingEnabled = true;
+			this.comboBox5.Location = new System.Drawing.Point(253, 117);
+			this.comboBox5.Name = "comboBox5";
+			this.comboBox5.Size = new System.Drawing.Size(121, 21);
+			this.comboBox5.TabIndex = 9;
+			// 
+			// label5
+			// 
+			this.label5.Location = new System.Drawing.Point(13, 115);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(100, 23);
+			this.label5.TabIndex = 8;
+			this.label5.Text = "Эргономика: ";
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(13, 150);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 10;
+			this.button1.Text = "Calculate";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// label6
+			// 
+			this.label6.Location = new System.Drawing.Point(13, 176);
+			this.label6.Name = "label6";
+			this.label6.Size = new System.Drawing.Size(258, 23);
+			this.label6.TabIndex = 11;
+			this.label6.Text = "Вероятность правильного выполнения задачи:";
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(277, 176);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(100, 20);
+			this.textBox1.TabIndex = 12;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(386, 261);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label6);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.comboBox5);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.comboBox4);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.comboBox3);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.comboBox2);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.comboBox1);
+			this.Controls.Add(this.label1);
+			this.Name = "MainForm";
+			this.Text = "Human reliability";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ Reliability/MainForm.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Acer
+ * Date: 30.09.2017
+ * Time: 17:17
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Collections.Generic;
+
+namespace LR1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : Form
+	{
+		Dictionary<string, double> operations = new Dictionary<string, double>();
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			operations.Add("Простая, рутинная", 0.001);
+			operations.Add("Требующая внимания, рутинная", 0.01);
+			operations.Add("Нестандартная", 0.1);
+			
+			operations.Add("2", 10);
+			operations.Add("10", 1);
+			operations.Add("20", 0.5);
+			operations.Add("3", 10);
+			operations.Add("30", 1);
+			operations.Add("45", 0.3);
+			operations.Add("60", 0.1);
+			
+			operations.Add("Тщательно отобран, хорошо тренирован", 3);
+			operations.Add("Средних знаний и тренировки", 2);
+			operations.Add("Мало знаний, слабо тренирован", 1);
+			
+			operations.Add("Аварийная ситуация", 3);
+			operations.Add("Потенциально аварийная ситуация", 2);
+			operations.Add("Нормальная ситуация", 1);
+			
+			operations.Add("Отличный микроклимат, отличное взаимодействие с объектом", 0.7);
+			operations.Add("Хороший микроклимат, хорошее взаимодействие", 1);
+			operations.Add("Натянутая обстановка и взаимодействие", 3);
+			operations.Add("Натянутая обстановка, плохое взаимодействие", 7);
+			operations.Add("Плохой микроклимат, плохое взаимодействие", 10);
+			
+			comboBox1.Items.Add("Простая, рутинная");
+			comboBox1.Items.Add("Требующая внимания, рутинная");
+			comboBox1.Items.Add("Нестандартная");
+			
+			comboBox2.Items.Add("2");
+			comboBox2.Items.Add("10");
+			comboBox2.Items.Add("20");
+			comboBox2.Items.Add("3");
+			comboBox2.Items.Add("30");
+			comboBox2.Items.Add("45");
+			comboBox2.Items.Add("60");
+			
+			comboBox3.Items.Add("Тщательно отобран, хорошо тренирован");
+			comboBox3.Items.Add("Средних знаний и тренировки");
+			comboBox3.Items.Add("Мало знаний, слабо тренирован");
+
+			comboBox4.Items.Add("Аварийная ситуация");
+			comboBox4.Items.Add("Потенциально аварийная ситуация");
+			comboBox4.Items.Add("Нормальная ситуация");
+			
+			comboBox5.Items.Add("Отличный микроклимат, отличное взаимодействие с объектом");
+			comboBox5.Items.Add("Хороший микроклимат, хорошее взаимодействие");
+			comboBox5.Items.Add("Натянутая обстановка и взаимодействие");
+			comboBox5.Items.Add("Натянутая обстановка, плохое взаимодействие");
+			comboBox5.Items.Add("Плохой микроклимат, плохое взаимодействие");
+			
+			//
+			// TODO: Add constructor code after the InitializeComponent() call.
+			//
+		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			double K=1;
+			if(comboBox1.Text!=""||comboBox2.Text!=""||comboBox3.Text!=""||comboBox4.Text!=""||comboBox5.Text!=""){
+				K=K*operations[comboBox1.Text]*operations[comboBox2.Text]*operations[comboBox3.Text];
+				K=K*operations[comboBox4.Text]*operations[comboBox5.Text];
+			}
+			double P = 1 - K;
+			textBox1.Text = P.ToString();
+		}
+	}
+}
